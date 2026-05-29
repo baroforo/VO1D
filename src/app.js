01c1971ff88a6728c16c596972110012e54ca671
baroforo <jkgoober1@gmail.com>
2026-05-29 10:08:52 +0200

    Update import path for setupLogs module

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { REST } from '@discordjs/rest';
 import express from 'express';
 import cron from 'node-cron';
 
-import setupLogs from './logs.js';
+import setupLogs from './commands/Utility/logs.js';
 import config from './config/application.js';
 import { initializeDatabase } from './utils/database.js';
 import { getGuildConfig } from './services/guildConfig.js';
